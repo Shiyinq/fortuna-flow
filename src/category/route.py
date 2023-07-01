29d24739fccde875dd0febaecdfa3cad6cb7c21c
Shiyinq <shiyinq.github.io@gmail.com>
2023-07-01 13:39:59 +0700

    add api get categories

--- src/category/route.py
+++ src/category/route.py
@@ -1,9 +1,19 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from src.category.schemas import CategoryCreate, CategoryCreateResponse
 from src import dependencies
 from src.category import service
 
 router = APIRouter()
+
+@router.get("/categories")
+async def get_categories(
+    page: int = Query(1), 
+    limit: int = Query(10),
+    current_user = Depends(dependencies.get_current_user)
+):
+    """Get list category for current user login"""
+    categories = await service.get_categories(current_user.userId, page, limit)
+    return categories
 
 @router.post("/category", status_code=201, response_model=CategoryCreateResponse)
 async def add_category(
