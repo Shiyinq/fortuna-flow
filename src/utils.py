5fe22345fb45ae5eea17bd54e23b1a927ab34e99
Shiyinq <shiyinq.github.io@gmail.com>
2023-07-01 13:36:39 +0700

    new util pagination

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+import math
+from typing import Any, Dict
+
+def pagination(total: int, page: int, limit: int) -> Dict[str, Any]:
+    total_pages = math.ceil(total / limit)
+    next_page = page + 1 if page < total_pages else None
+    prev_page = page - 1 if page > 1 else None
+
+    return   {
+        "page": page,
+        "limit": limit,
+        "prevPage": prev_page,
+        "nextPage": next_page,
+        "totalPage": total_pages
+    }
