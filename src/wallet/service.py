96482187a78cee1ab911cc6afb63cecec43b5a05
Shiyinq <shiyinq.github.io@gmail.com>
2023-07-01 13:41:29 +0700

    import pagination from utils

--- src/wallet/service.py
+++ src/wallet/service.py
@@ -4,19 +4,7 @@ from src.db import database
 from src.wallet.constants import Info
 from src.wallet.schemas import WalletCreate
 from src.wallet.exceptions import WalletNotFound
-
-def pagination(total: int, page: int, limit: int) -> Dict[str, Any]:
-    total_pages = math.ceil(total / limit)
-    next_page = page + 1 if page < total_pages else None
-    prev_page = page - 1 if page > 1 else None
-
-    return   {
-        "page": page,
-        "limit": limit,
-        "prevPage": prev_page,
-        "nextPage": next_page,
-        "totalPage": total_pages
-    }
+from src.utils import pagination
 
 async def get_wallets(user_id: str, page: int, limit: int) -> Dict[str, Any]:
     skip = (page - 1) * limit
