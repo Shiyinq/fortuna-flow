6401a63a767e9c383593d865e35ad9dc69717a95
Shiyinq <shiyinq.github.io@gmail.com>
2023-07-10 22:31:55 +0700

    add mongodb_uri

--- src/config.py
+++ src/config.py
@@ -10,6 +10,7 @@ class Config:
         return cls._instance
 
     def __init__(self):
+        self.mongo_uri = os.getenv("MONGODB_URI")
         self.secret_key = os.getenv(
             "SECRET_KEY",
             "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7",
