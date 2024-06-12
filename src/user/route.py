e47164e228d85e32fa126c67d4deb920fdf588e1
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-12 18:56:25 +0700

    feat: add /users/ path to endpoint

--- src/user/route.py
+++ src/user/route.py
@@ -8,14 +8,14 @@ from src.user.schemas import UserCreate, UserCreateResponse
 router = APIRouter()
 
 
-@router.post("/signup", status_code=201, response_model=UserCreateResponse)
+@router.post("/users/signup", status_code=201, response_model=UserCreateResponse)
 async def signup(user: UserCreate):
     """Create new user"""
     new_user = await service.create_user(user)
     return new_user
 
 
-@router.get("/profile", response_model=UserCurrent)
+@router.get("/users/profile", response_model=UserCurrent)
 async def user_profile(current_user=Depends(dependencies.get_current_user)):
     """Get info current user login"""
     return current_user
