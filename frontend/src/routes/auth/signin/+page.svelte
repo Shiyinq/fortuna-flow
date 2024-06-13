9ed54b53f2db057aeddbf4e281176f4983f32283
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-13 20:39:07 +0700

    feat: add page auth/signin and auth/signin, and auth.css

---
+++ frontend/src/routes/auth/signin/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import '../auth.css';
+</script>
+
+<div class="auth sign-in">
+	<form class="form">
+		<h1>Sign in</h1>
+		<p>Welcome to Fotuna Flow</p>
+		<div class="form-field">
+			<input
+				class="nb-input default"
+				type="text"
+				name="username"
+				id="username"
+				placeholder="Username"
+			/>
+		</div>
+		<div class="form-field">
+			<input
+				class="nb-input default"
+				type="password"
+				name="password"
+				id="password"
+				placeholder="Password"
+			/>
+		</div>
+		<div class="form-button">
+			<button class="nb-button default" type="submit" name="signin">Sign in</button>
+		</div>
+		<p class="link-auth">
+			Don't have an account? <a href="/auth/signup">Sign up</a>
+		</p>
+	</form>
+</div>
---
+++ frontend/src/routes/auth/signup/+page.svelte
@@ -0,0 +1,69 @@
+<script>
+	import '../auth.css';
+</script>
+
+<div class="auth sign-in">
+	<form class="form">
+		<h1>Sign up</h1>
+		<p>Create an account before sign in</p>
+		<div class="form-field">
+			<input
+				class="nb-input default"
+				type="text"
+				name="name"
+				id="name"
+				placeholder="Enter your name"
+			/>
+		</div>
+		<div class="form-field">
+			<input
+				class="nb-input default"
+				type="text"
+				name="username"
+				id="username"
+				placeholder="Enter your username"
+			/>
+		</div>
+		<div class="form-field">
+			<input
+				class="nb-input default"
+				type="email"
+				name="email"
+				id="email"
+				placeholder="Enter your email"
+			/>
+		</div>
+		<div class="form-field">
+			<input
+				class="nb-input default"
+				type="password"
+				name="password"
+				id="password"
+				placeholder="Enter your password"
+			/>
+		</div>
+		<div class="form-field">
+			<input
+				class="nb-input default"
+				type="password"
+				name="confirm-password"
+				id="confirm-password"
+				placeholder="Re enter your password"
+			/>
+		</div>
+		<div class="form-button">
+			<button class="nb-button default" type="submit" name="signin">Sign in</button>
+		</div>
+		<p class="link-auth">
+			Already have an account? <a href="/auth/signin">Sign in</a>
+		</p>
+	</form>
+</div>
+
+<style>
+	.auth {
+		height: 80vh;
+		display: flex;
+		justify-content: center;
+	}
+</style>
