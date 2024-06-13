dc81e4baa1d20970f773f2c97a10d8b25b3d5f15
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-13 21:02:25 +0700

    feat: add sign-in buttons for Google and GitHub

--- frontend/src/routes/auth/signin/+page.svelte
+++ frontend/src/routes/auth/signin/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import '../auth.css';
+	import github from '$lib/images/github.svg';
+	import google from '$lib/images/google.svg';
 </script>
 
 <div class="auth sign-in">
@@ -31,4 +33,53 @@
 			Don't have an account? <a href="/auth/signup">Sign up</a>
 		</p>
 	</form>
+
+	<div class="optional-sign-in">
+		<button class="nb-button default" name="github">
+			<img src={github} alt="GitHub" />
+			<span>Sign in with GitHub</span>
+		</button>
+		<button class="nb-button default" name="signin">
+			<img class="img-google" src={google} alt="Google" />
+			<span>Sign in with Google</span>
+		</button>
+	</div>
 </div>
+
+<style>
+	.optional-sign-in {
+		width: 35%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		margin-top: 16px;
+	}
+
+	.optional-sign-in button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin-bottom: 8px;
+		padding: 8px;
+		width: 100%;
+		box-sizing: border-box;
+	}
+
+	.optional-sign-in img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+		margin-right: 8px;
+	}
+
+	.img-google {
+		width: 1.5em !important;
+		height: 1.5em !important;
+	}
+
+	.optional-sign-in span {
+		display: inline-block;
+		vertical-align: middle;
+	}
+</style>
