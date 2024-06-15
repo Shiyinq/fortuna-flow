abe71a1f8d76d4124753d46f27a01eb33901ca73
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-15 12:34:03 +0700

    feat: update  home page display total balance, wallets, recent total spents and recent transactions

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,59 +1,164 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import wallet from '$lib/images/wallet.svg';
+	import BarChart from '$lib/components/charts/BarChart.svelte';
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Fortuna Flow" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<div class="home">
+	<div class="total-balance">
+		<h3>Rp 1.429.732,42</h3>
+		<span>Total balance</span>
+	</div>
 
-		to your new<br />SvelteKit app
-	</h1>
+	<br />
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+	<div class="wallets">
+		<div class="wallet-header">
+			<h5>My Wallets</h5>
+			<a href="/wallets"><h6>See all</h6></a>
+		</div>
+		<div class="wallet-info">
+			<div class="wallet-title">
+				<img class="img-google" src={wallet} alt="Google" />
+				<span>ATM</span>
+			</div>
+			<div class="wallet-balance">
+				<span>Rp 298.352,42</span>
+			</div>
+		</div>
+		<div class="wallet-info">
+			<div class="wallet-title">
+				<img class="img-google" src={wallet} alt="Google" />
+				<span>E-wallet</span>
+			</div>
+			<div class="wallet-balance">
+				<span>Rp 598.352,42</span>
+			</div>
+		</div>
+		<div class="wallet-info">
+			<div class="wallet-title">
+				<img class="img-google" src={wallet} alt="Google" />
+				<span>Cash</span>
+			</div>
+			<div class="wallet-blance">
+				<span>Rp 100.000</span>
+			</div>
+		</div>
+	</div>
 
-	<Counter />
-</section>
+	<br />
+
+	<div class="total-spent">
+		<div class="total-spent-header">
+			<h5>Recent total spents</h5>
+		</div>
+		<BarChart />
+	</div>
+
+	<br />
+
+	<div class="wallets">
+		<div class="wallet-header">
+			<h5>Recent transactions</h5>
+			<a href="/wallets"><h6>See all</h6></a>
+		</div>
+		<div class="wallet-info">
+			<div class="wallet-title">
+				<img class="img-google" src={wallet} alt="Google" />
+				<span>Ojol</span>
+			</div>
+			<div class="wallet-balance">
+				<span>Rp 298.352,42</span>
+			</div>
+		</div>
+		<div class="wallet-info">
+			<div class="wallet-title">
+				<img class="img-google" src={wallet} alt="Google" />
+				<span>Makan siang</span>
+			</div>
+			<div class="wallet-balance">
+				<span>Rp 598.352,42</span>
+			</div>
+		</div>
+		<div class="wallet-info">
+			<div class="wallet-title">
+				<img class="img-google" src={wallet} alt="Google" />
+				<span>Beli skin care</span>
+			</div>
+			<div class="wallet-blance">
+				<span>Rp 100.000</span>
+			</div>
+		</div>
+	</div>
+</div>
 
 <style>
-	section {
+	.home {
+		margin-top: 24px;
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
 		align-items: center;
-		flex: 0.6;
+		flex-direction: column;
 	}
 
-	h1 {
-		width: 100%;
+	.total-balance {
+		width: 320px;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	.total-spent {
+		width: 320px;
+		padding: 10px;
+		border-radius: 8px;
+		border: 1px solid rgba(255, 255, 255, 0.7);
 	}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	h3 {
+		padding: 0;
+		margin: 0;
+	}
+	h5,
+	h6 {
+		margin-top: 0;
+	}
+	.wallets {
+		width: 320px;
+		padding: 10px;
+		border-radius: 8px;
+		border: 1px solid rgba(255, 255, 255, 0.7);
+	}
+
+	.wallet-header {
+		width: 300px;
+		display: flex;
+		margin-top: 8px;
+		justify-content: space-between;
+	}
+
+	.wallet-info {
+		width: 300px;
+		padding: 12px;
+		display: flex;
+		border-radius: 8px;
+		margin-bottom: 8px;
+		justify-content: space-between;
+		background-color: rgba(255, 255, 255, 0.7);
+	}
+
+	.wallet-title {
+		display: flex;
+	}
+
+	span {
+		font-size: 13px;
+	}
+
+	img {
+		width: 1em;
+		height: 1em;
+		object-fit: contain;
+		margin-right: 8px;
 	}
 </style>
