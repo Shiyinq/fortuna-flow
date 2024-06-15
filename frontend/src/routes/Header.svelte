ae633ff7963bd1988c5a4ddc01d8c92bc9098701
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-15 15:27:09 +0700

    feat: change url github and add target blank

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -32,7 +32,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/Shiyinq/fortuna-flow" target="_blank">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
