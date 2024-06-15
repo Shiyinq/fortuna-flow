d1c8c4eea475a27cfb0a8c7e045eb499df112edc
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-15 12:30:39 +0700

    feat: add BarChart component

---
+++ frontend/src/lib/components/charts/BarChart.svelte
@@ -0,0 +1,51 @@
+<script>
+	import { Bar } from 'svelte-chartjs';
+    import { Chart, registerables } from 'chart.js';
+
+    Chart.register(...registerables);
+
+	let data = {
+		labels: ['March', 'April', 'May'],
+		datasets: [
+			{
+				label: 'Total Spent',
+				data: [1302401, 4250250, 2500000],
+				backgroundColor: [
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: ['rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+				borderWidth: 1
+			}
+		]
+	};
+
+	let options = {
+		responsive: true,
+		scales: {
+			y: {
+				beginAtZero: true,
+                display: false
+			}
+		},
+		plugins: {
+            legend: {
+                display: false
+            }
+        }
+	};
+</script>
+
+<div class="chart-container" style="position: relative; ">
+	<Bar {data} {options} />
+</div>
+
+<style>
+	.chart-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 20px auto;
+	}
+</style>
