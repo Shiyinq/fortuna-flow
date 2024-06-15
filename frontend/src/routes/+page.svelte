b7cc7cd7b46b8c1e1ad75fe3e466b5a97a6832f4
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-15 15:18:32 +0700

    refactor: use component

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
-	import wallet from '$lib/images/wallet.svg';
 	import BarChart from '$lib/components/charts/BarChart.svelte';
+	import MyWallets from '$lib/components/wallets/MyWallets.svelte';
+	import RecentTransactions from '$lib/components/transactions/RecentTransactions.svelte';
 </script>
 
 <svelte:head>
@@ -16,39 +17,7 @@
 
 	<br />
 
-	<div class="wallets">
-		<div class="wallet-header">
-			<h5>My Wallets</h5>
-			<a href="/wallets"><h6>See all</h6></a>
-		</div>
-		<div class="wallet-info">
-			<div class="wallet-title">
-				<img class="img-google" src={wallet} alt="Google" />
-				<span>ATM</span>
-			</div>
-			<div class="wallet-balance">
-				<span>Rp 298.352,42</span>
-			</div>
-		</div>
-		<div class="wallet-info">
-			<div class="wallet-title">
-				<img class="img-google" src={wallet} alt="Google" />
-				<span>E-wallet</span>
-			</div>
-			<div class="wallet-balance">
-				<span>Rp 598.352,42</span>
-			</div>
-		</div>
-		<div class="wallet-info">
-			<div class="wallet-title">
-				<img class="img-google" src={wallet} alt="Google" />
-				<span>Cash</span>
-			</div>
-			<div class="wallet-blance">
-				<span>Rp 100.000</span>
-			</div>
-		</div>
-	</div>
+	<MyWallets />
 
 	<br />
 
@@ -61,39 +30,7 @@
 
 	<br />
 
-	<div class="wallets">
-		<div class="wallet-header">
-			<h5>Recent transactions</h5>
-			<a href="/wallets"><h6>See all</h6></a>
-		</div>
-		<div class="wallet-info">
-			<div class="wallet-title">
-				<img class="img-google" src={wallet} alt="Google" />
-				<span>Ojol</span>
-			</div>
-			<div class="wallet-balance">
-				<span>Rp 298.352,42</span>
-			</div>
-		</div>
-		<div class="wallet-info">
-			<div class="wallet-title">
-				<img class="img-google" src={wallet} alt="Google" />
-				<span>Makan siang</span>
-			</div>
-			<div class="wallet-balance">
-				<span>Rp 598.352,42</span>
-			</div>
-		</div>
-		<div class="wallet-info">
-			<div class="wallet-title">
-				<img class="img-google" src={wallet} alt="Google" />
-				<span>Beli skin care</span>
-			</div>
-			<div class="wallet-blance">
-				<span>Rp 100.000</span>
-			</div>
-		</div>
-	</div>
+	<RecentTransactions />
 </div>
 
 <style>
@@ -119,46 +56,12 @@
 		padding: 0;
 		margin: 0;
 	}
-	h5,
-	h6 {
+
+	h5 {
 		margin-top: 0;
-	}
-	.wallets {
-		width: 320px;
-		padding: 10px;
-		border-radius: 8px;
-		border: 1px solid rgba(255, 255, 255, 0.7);
-	}
-
-	.wallet-header {
-		width: 300px;
-		display: flex;
-		margin-top: 8px;
-		justify-content: space-between;
-	}
-
-	.wallet-info {
-		width: 300px;
-		padding: 12px;
-		display: flex;
-		border-radius: 8px;
-		margin-bottom: 8px;
-		justify-content: space-between;
-		background-color: rgba(255, 255, 255, 0.7);
-	}
-
-	.wallet-title {
-		display: flex;
 	}
 
 	span {
 		font-size: 13px;
-	}
-
-	img {
-		width: 1em;
-		height: 1em;
-		object-fit: contain;
-		margin-right: 8px;
 	}
 </style>
