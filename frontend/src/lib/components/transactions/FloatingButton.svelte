b31da427dd3cd895e678db3fbe15c7e772faba59
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-17 13:51:36 +0700

    feat: add floating button component

---
+++ frontend/src/lib/components/transactions/FloatingButton.svelte
@@ -0,0 +1,65 @@
+<script>
+    export let label = "+";
+    export let handleClick = () => {};
+  
+    let isVisible = false;
+  
+    const toggleVisibility = () => {
+      isVisible = !isVisible;
+    }
+  </script>
+  
+  <div class="floating-button-container">
+    <button class="floating-button" on:click={toggleVisibility}>
+      {label}
+    </button>
+  
+    {#if isVisible}
+      <div class="options">
+        <button on:click={handleClick}>Option 1</button>
+        <button on:click={handleClick}>Option 2</button>
+      </div>
+    {/if}
+  </div>
+  
+  <style>
+    .floating-button-container {
+      position: fixed;
+      bottom: 50px;
+      right: 20px;
+      z-index: 999;
+    }
+  
+    .floating-button {
+      background-color: var(--color-theme-1);
+      color: #fff;
+      border: none;
+      border-radius: 50%;
+      width: 50px;
+      height: 50px;
+      cursor: pointer;
+      box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+    }
+  
+    .options {
+      position: absolute;
+      bottom: 60px;
+      right: 0;
+      background-color: #fff;
+      border-radius: 5px;
+      box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+      padding: 5px;
+    }
+  
+    .options button {
+      display: block;
+      margin-bottom: 5px;
+      padding: 5px 10px;
+      background-color: var(--color-theme-1);
+      color: #fff;
+      border: none;
+      border-radius: 3px;
+      cursor: pointer;
+    }
+  </style>
+  
