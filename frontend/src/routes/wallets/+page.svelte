710c3aff1a35d0ce876e63f7b9e212d812e46952
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-17 13:56:21 +0700

    feat: add wallets page

---
+++ frontend/src/routes/wallets/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+	import wallet from '$lib/images/wallet.svg';
+	import WalletInfo from '$lib/components/wallets/WalletInfo.svelte';
+
+    let wallets = [
+        {
+            icon: wallet,
+            title: 'ATM',
+            balance: '10.200.000'
+        },
+        {
+            icon: wallet,
+            title: 'E-wallet',
+            balance: '598.352,42'
+        },
+        {
+            icon: wallet,
+            title: 'Cash',
+            balance: '100.000'
+        },
+        {
+            icon: wallet,
+            title: 'Investment',
+            balance: '2.500.000.000'
+        },
+        {
+            icon: wallet,
+            title: 'Savings',
+            balance: '5.000.000'
+        },
+        {
+            icon: wallet,
+            title: 'Crypto',
+            balance: '1.000.000.000'
+        },
+        {
+            icon: wallet,
+            title: 'Credit Card',
+            balance: '0'
+        }
+    ];
+
+</script>
+
+<div class="wallets">
+	<div class="wallet-header">
+		<h5>My Wallets</h5>
+		<a href="/wallets"><h6>Create New Wallet</h6></a>
+	</div>
+	{#each wallets as wallet}
+		<WalletInfo icon={wallet.icon} title={wallet.title} balance={wallet.balance} />
+	{/each}
+</div>
+
+<style>
+	h5,
+	h6 {
+		margin-top: 0;
+	}
+	.wallets {
+		width: 100%;
+		padding: 10px;
+		border-radius: 8px;
+		border: 1px solid rgba(255, 255, 255, 0.7);
+	}
+
+	.wallet-header {
+		width: 100%;
+		display: flex;
+		margin-top: 8px;
+		justify-content: space-between;
+	}
+</style>
