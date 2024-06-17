918485087cad2084c63a8897b186c26bfa313c3a
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-17 14:08:57 +0700

    add function to convert number to emoji

---
+++ frontend/src/lib/utils/index.ts
@@ -0,0 +1,14 @@
+export const numberToEmoji = (number: number) => {
+	const emojis = ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'];
+	const numString = number.toString();
+	let result = '';
+
+	for (let i = 0; i < numString.length; i++) {
+		const digit = parseInt(numString[i]);
+		if (!isNaN(digit)) {
+			result += emojis[digit];
+		}
+	}
+
+	return result;
+};
