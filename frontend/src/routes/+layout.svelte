c53e3994f2078a6c712e0a994e1492f3e16c182d
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-17 21:27:41 +0700

    feat: add preloading indicator

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,14 +1,18 @@
 <script>
 	import './styles.css';
 	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
 	import { token } from '$lib/store';
+	import { goto } from '$app/navigation';
+	import { navigating } from '$app/stores';
 
 	import Header from '$lib/components/layout/Header.svelte';
 	import Footer from '$lib/components/layout/Footer.svelte';
 	import SplashScreen from '$lib/components/SplashScreen.svelte';
+	import PreloadingIndicator from '$lib/components/PreloadingIndicator.svelte';
+
 
 	let showSplash = true;
+	let isNavigating = false;
 
 	onMount(() => {
 		if (!$token) {
@@ -18,7 +22,22 @@
 			showSplash = false;
 		}, 2000);
 	});
+
+	// Sometimes, this may not work because the page moves too quickly during navigation.
+	$: if ($navigating) {
+		isNavigating = true;
+		// console.log('Navigating started', isNavigating);
+		$navigating.complete.then(() => {
+			isNavigating = false;
+			// console.log('Navigating ended', isNavigating);
+		});
+	}
+
 </script>
+
+{#if isNavigating}
+	<PreloadingIndicator />
+{/if}
 
 {#if showSplash}
 	<SplashScreen show={showSplash} />
