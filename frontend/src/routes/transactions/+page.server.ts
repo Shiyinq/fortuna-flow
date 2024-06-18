f61fd3dfc70214dbe73dfa51ef1d4402ccd7ac2b
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-18 12:03:41 +0700

    feat: get dynamic data all transactions

---
+++ frontend/src/routes/transactions/+page.server.ts
@@ -0,0 +1,19 @@
+import { loadWithToken } from '$lib/utils/loadPage.js';
+import type { RequestEvent } from '@sveltejs/kit';
+
+import { getAllTransactions } from '$lib/apis/transactions';
+import { getCurrentMonth } from '$lib/utils';
+
+const loadData = async (token: string) => {
+	const [transactions] = await Promise.all([
+		getAllTransactions(token, 1, 32, getCurrentMonth())
+	]);
+
+	return {
+		transactions
+	};
+};
+
+export const load = async (loadFunction: RequestEvent) => {
+	return await loadWithToken(loadFunction, loadData);
+};
--- frontend/src/routes/transactions/+page.svelte
+++ frontend/src/routes/transactions/+page.svelte
@@ -1,214 +1,10 @@
 <script lang="ts">
-	import wallet from '$lib/images/wallet.svg';
 	import CurrentWallet from '$lib/components/wallets/CurrentWallet.svelte';
 	import MonthSelector from '$lib/components/transactions/MonthSelector.svelte';
 	import TransactionsInfo from '$lib/components/transactions/TransactionsInfo.svelte';
+	import { formatCurrency, formatDate } from '$lib/utils';
 
-	let transactions = [
-		{
-			transactionDate: '2024-06-15',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Transportation',
-					description: 'Ojol',
-					date: 'Sat, 15 June 2024',
-					amount: '220.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-14',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Food',
-					description: 'Makan siang',
-					date: 'Fri, 14 June 2024',
-					amount: '120.352',
-					type: 'expense'
-				},
-				{
-					icon: wallet,
-					category: 'Gift',
-					description: 'Hadiah',
-					date: 'Fri, 14 June 2024',
-					amount: '520.000',
-					type: 'income'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-13',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Self Care',
-					description: 'Skin care',
-					date: 'Thu, 13 June 2024',
-					amount: '560.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-12',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Utilities',
-					description: 'Electricity Bill',
-					date: 'Wed, 12 June 2024',
-					amount: '300.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-11',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Entertainment',
-					description: 'Movie Ticket',
-					date: 'Tue, 11 June 2024',
-					amount: '150.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-10',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Food',
-					description: 'Groceries',
-					date: 'Mon, 10 June 2024',
-					amount: '450.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-09',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Health',
-					description: 'Doctor Appointment',
-					date: 'Sun, 09 June 2024',
-					amount: '700.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-08',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Income',
-					description: 'Salary',
-					date: 'Sat, 08 June 2024',
-					amount: '5000.000',
-					type: 'income'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-07',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Transportation',
-					description: 'Taxi',
-					date: 'Fri, 07 June 2024',
-					amount: '200.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-06',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Self Care',
-					description: 'Massage',
-					date: 'Thu, 06 June 2024',
-					amount: '300.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-05',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Food',
-					description: 'Dinner',
-					date: 'Wed, 05 June 2024',
-					amount: '250.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-04',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Utilities',
-					description: 'Water Bill',
-					date: 'Tue, 04 June 2024',
-					amount: '100.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-03',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Transportation',
-					description: 'Train Ticket',
-					date: 'Mon, 03 June 2024',
-					amount: '75.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-02',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Food',
-					description: 'Lunch',
-					date: 'Sun, 02 June 2024',
-					amount: '125.000',
-					type: 'expense'
-				}
-			]
-		},
-		{
-			transactionDate: '2024-06-01',
-			datas: [
-				{
-					icon: wallet,
-					category: 'Gift',
-					description: 'Birthday Gift',
-					date: 'Sat, 01 June 2024',
-					amount: '400.000',
-					type: 'expense'
-				}
-			]
-		}
-	];
+	export let data: any;
 </script>
 
 <svelte:head>
@@ -221,19 +17,19 @@
 	<br />
 	<MonthSelector />
 	<br />
-	{#each transactions as { transactionDate, datas }}
+	{#each data.transactions.data as { transactionDate, transactions, totalAmountExpense, totalAmountIncome }}
 		<div class="transactions-card">
 			<div class="transactions-header">
-				<h5>{transactionDate}</h5>
-				<!-- <a href="/recent-transactions"><h6>See all</h6></a> -->
+				<h5>{formatDate(transactionDate)}</h5>
+				<h5>{formatCurrency(totalAmountIncome - totalAmountExpense)}</h5>
 			</div>
-			{#each datas as data}
+			{#each transactions as transaction}
 				<TransactionsInfo
-					icon={data.icon}
-					category={data.category}
-					description={data.description}
-					amount={data.amount}
-					type={data.type}
+					icon={transaction.categoryDetail.categoryIcon}
+					category={transaction.categoryDetail.name}
+					description={transaction.note}
+					amount={formatCurrency(transaction.amount)}
+					type={transaction.type}
 				/>
 			{/each}
 		</div>
