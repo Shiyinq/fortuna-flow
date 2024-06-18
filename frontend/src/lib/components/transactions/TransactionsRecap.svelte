f5b10afa1d69dd66d1e1d83a82c5b26d7f05e6a2
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-18 21:03:47 +0700

    feat: add transactions recap component

---
+++ frontend/src/lib/components/transactions/TransactionsRecap.svelte
@@ -0,0 +1,44 @@
+<div class="transactions-recap">
+    <div class="recap-inflow">
+        <h5>Inflow</h5>
+        <h5>Rp 100.000</h5>
+    </div>
+    <div class="recap-outflow">
+        <h5>Outflow</h5>
+        <h5>Rp 200.000</h5>
+    </div>
+    <div class="recap-total">
+        <h5>Rp -100.000</h5>
+    </div>
+</div>
+
+<style>
+    h5 {
+		margin-top: 0;
+		margin-bottom: 0;
+	}
+
+    .transactions-recap {
+		width: 100%;
+		display: flex;
+		padding: 10px;
+		border-radius: 8px;
+		flex-direction: column;
+		border: 1px solid var(	--color-bg-0);
+		background-color: #fff;
+	}
+
+	.transactions-recap .recap-inflow, .transactions-recap .recap-outflow {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.transactions-recap .recap-total {
+		margin-top: 8px;
+		text-align: right;
+	}
+
+	.transactions-recap .recap-outflow {
+		border-bottom: 1px solid var(--color-bg-0);
+	}
+</style>
