6997ec9770c8006d1aa5f4524b3ee7bba7313849
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-18 08:41:38 +0700

    feat: centralize fetch requests with token and change format currency

--- frontend/src/lib/utils/index.ts
+++ frontend/src/lib/utils/index.ts
@@ -1,3 +1,25 @@
+import { FORTUNA_API_BASE_URL } from '$lib/constants';
+
+export const myFetch = async (
+	method: string,
+	token: string,
+	endpoint: string,
+	options?: RequestInit
+) => {
+	const headers = {
+		...options?.headers,
+		Authorization: `Bearer ${token}`
+	};
+
+	delete options?.headers;
+	return await fetch(`${FORTUNA_API_BASE_URL}${endpoint}`, {
+		...{ method: method },
+		headers,
+		...options
+	});
+};
+
+
 export const numberToEmoji = (number: number) => {
 	const emojis = ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'];
 	const numString = number.toString();
@@ -12,3 +34,20 @@ export const numberToEmoji = (number: number) => {
 
 	return result;
 };
+
+export const formatCurrency = (amount: number, currencySymbol: string = 'Rp') => {
+    let locale: string;
+    
+    switch (currencySymbol) {
+        case '$':
+            locale = 'en-US';
+            break;
+        case 'Rp':
+        default:
+            locale = 'id-ID';
+            break;
+    }
+
+    const formattedAmount = `${currencySymbol} ${amount.toLocaleString(locale)}`;
+    return formattedAmount;
+}
