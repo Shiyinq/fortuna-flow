ebcd1f9d687b0c2e10e39e578029f42d8cfe21af
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-22 15:32:22 +0700

    feat: add api analytics get total transactions

---
+++ frontend/src/lib/apis/analytics/index.ts
@@ -0,0 +1,11 @@
+import { myFetch } from '$lib/utils';
+
+export const getTotalTransactions = async (token: string, start_date: string, end_date: string) => {
+	const response = await myFetch(
+		'GET',
+		token,
+		`/analytics/recent-transactions?start_date=${start_date}&end_date=${end_date}`
+	);
+	if (!response.ok) throw await response.json();
+	return await response.json();
+};
