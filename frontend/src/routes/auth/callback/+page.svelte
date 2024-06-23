c6b5525e1b455115e2deaad31d0e1aa12288766d
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-23 18:47:01 +0700

    feat: implement callback page for GitHub and Google OAuth

---
+++ frontend/src/routes/auth/callback/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { goto } from '$app/navigation';
+	import { token } from '$lib/store';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        const params = new URLSearchParams(window.location.search);
+        const access_token = params.get('access_token');
+
+        if (access_token) {
+            token.set(access_token);
+            goto('/');
+        } else {
+            goto('/auth/signin');
+        }
+    });
+</script>
+
+<p class="check-callback">Authenticating...</p>
+
+<style>
+    .check-callback {
+        height: 50vh;
+        display: flex;
+        text-align: center;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
