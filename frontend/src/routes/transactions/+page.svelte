abfc7d87e391172d7897b66135b69b939b21e671
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-25 21:36:26 +0700

    feat: add button add transaction for desktop mode & mobile mode

--- frontend/src/routes/transactions/+page.svelte
+++ frontend/src/routes/transactions/+page.svelte
@@ -12,6 +12,8 @@
 	import TransactionsInfo from '$lib/components/transactions/TransactionsInfo.svelte';
 	import TransactionsRecap from '$lib/components/transactions/TransactionsRecap.svelte';
 
+	import FloatingButton from '$lib/components/transactions/FloatingButton.svelte';
+
 	export let data: any;
 
 	let activeTransactions: any = [];
@@ -54,6 +56,9 @@
 	<br />
 	<TransactionsRecap transactions={activeTransactions} />
 	<br />
+	<div class="add-transaction">
+		<a href="/transactions/create">+ Add Transaction</a>
+	</div>
 	{#if !activeTransactions.length}
 		<EmptyState />
 	{/if}
@@ -75,9 +80,28 @@
 		</div>
 		<br />
 	{/each}
+
+	<FloatingButton />
 </div>
 
 <style>
+	.add-transaction {
+		width: 100%;
+		display: flex;
+		margin-bottom: 20px;
+	}
+
+	.add-transaction a {
+		width: 100%;
+		padding: 8px;
+		cursor: pointer;
+		color: #fafafa;
+		text-align: center;
+		border-radius: 4px;
+		border: 1px solid var(--color-bg-0);
+		background-color: var(--color-theme-1);
+	}
+
 	.transactions {
 		display: flex;
 		align-items: center;
@@ -102,5 +126,11 @@
 		margin-top: 8px;
 		margin-bottom: 4px;
 		justify-content: space-between;
+	}
+
+	@media only screen and (max-width: 480px) {
+		.add-transaction {
+			display: none;
+		}
 	}
 </style>
