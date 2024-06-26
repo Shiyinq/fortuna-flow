dedc7b9122290e12933d39e963f9951f43669a1e
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-26 09:14:38 +0700

    feat: component add transaction button

---
+++ frontend/src/lib/components/transactions/AddTransactionButton.svelte
@@ -0,0 +1,28 @@
+<div class="add-transaction">
+    <a href="/transactions/create">+ Add Transaction</a>
+</div>
+
+<style>
+    .add-transaction {
+		width: 100%;
+		display: flex;
+		margin-bottom: 20px;
+	}
+
+	.add-transaction a {
+		width: 100%;
+		padding: 8px;
+		cursor: pointer;
+		color: #fafafa;
+		text-align: center;
+		border-radius: 4px;
+		border: 1px solid var(--color-bg-0);
+		background-color: var(--color-theme-1);
+	}
+
+	@media only screen and (max-width: 480px) {
+		.add-transaction {
+			display: none;
+		}
+	}
+</style>
