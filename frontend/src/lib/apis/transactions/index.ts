8dbef3a9a5a99e56ae00b9d4f5bd3314358db165
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-26 07:45:22 +0700

    feat: add API add transaction

--- frontend/src/lib/apis/transactions/index.ts
+++ frontend/src/lib/apis/transactions/index.ts
@@ -1,5 +1,33 @@
 import { myFetch } from '$lib/utils';
 
+export const addTransaction = async (
+	token: string,
+	walletId: string,
+	categoryId: string,
+	amount: number,
+	type: string,
+	note: string,
+	transactionDate: string
+) => {
+	const response = await myFetch('POST', token, `/transactions`, {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			walletId,
+			categoryId,
+			amount,
+			type,
+			note,
+			transactionDate
+		})
+	});
+	if (!response.ok) throw await response.json();
+	return await response.json();
+};
+
 export const getRecentTransactions = async (token: string, limit: number = 5) => {
 	const response = await myFetch('GET', token, `/transactions/recent?limit=${limit}`);
 	if (!response.ok) throw await response.json();
