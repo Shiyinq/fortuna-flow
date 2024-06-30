56391c569d06e788c9657e5f768bca7b7d43ff4b
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-30 15:08:26 +0700

    feat: add pie chart component

---
+++ frontend/src/lib/components/charts/PieChart.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import { Pie } from 'svelte-chartjs';
+    import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale } from 'chart.js';
+
+    import EmptyState from '$lib/components/EmptyState.svelte';
+    
+    export let data: any;
+    ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+
+    const dataChart = {
+      labels: data.labels,
+      datasets: [
+        {
+          data: data.data,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.8)',
+            'rgba(54, 162, 235, 0.8)',
+            'rgba(255, 206, 86, 0.8)',
+            'rgba(75, 192, 192, 0.8)',
+            'rgba(153, 102, 255, 0.8)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+  
+    const options = {
+      responsive: true
+    };
+  </script>
+
+{#if data.data.length == 0}
+    <EmptyState />
+{:else}
+    <Pie data={dataChart} {options} />
+{/if}
