2027869bf2e6a9de9a16bf06796a6b64db83dfac
Shiyinq <shiyinq.github.io@gmail.com>
2024-07-01 20:47:49 +0700

    feat: add component heatmap

---
+++ frontend/src/lib/components/charts/Heatmap.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import SvelteHeatmap from 'svelte-heatmap';
+
+	export let data:any = []
+	export let startDate = '';
+	export let endDate = '';
+
+</script>
+
+<div class="container">
+	<SvelteHeatmap
+		allowOverflow={true}
+		cellGap={5}
+		cellRadius={1}
+		colors={['#00C853', '#33FF99', '#00E064', '#009933']}
+		data={data}
+		dayLabelWidth={20}
+		emptyColor={'#ecedf0'}
+		{endDate}
+		monthGap={20}
+		monthLabelHeight={20}
+		{startDate}
+		view={'monthly'}
+	/>
+</div>
+
+<style>
+	.container {
+		margin-top: 16px;
+		border-radius: 8px;
+		padding: 20px;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		background-color: #fff;
+	}
+</style>
