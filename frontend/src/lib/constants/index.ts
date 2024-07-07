c0805e788aa62c92e7186a944390c2484ab1b66a
Shiyinq <shiyinq.github.io@gmail.com>
2024-07-07 07:37:02 +0700

    feat: change and add varaible env

--- frontend/src/lib/constants/index.ts
+++ frontend/src/lib/constants/index.ts
@@ -1,3 +1,6 @@
-import { PUBLIC_FORTUNA_API_BASE_URL } from '$env/static/public';
+import { browser } from '$app/environment';
+import { PUBLIC_CLIENT_SIDE_FORTUNA_API_BASE_URL, PUBLIC_SERVER_SIDE_FORTUNA_API_BASE_URL } from '$env/static/public';
 
-export const FORTUNA_API_BASE_URL = PUBLIC_FORTUNA_API_BASE_URL;
+
+console.log(browser)
+export const FORTUNA_API_BASE_URL = browser ? PUBLIC_CLIENT_SIDE_FORTUNA_API_BASE_URL : PUBLIC_SERVER_SIDE_FORTUNA_API_BASE_URL;
