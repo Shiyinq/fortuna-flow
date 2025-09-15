a938d2f17a197f2163203ba67726da4e25833a58
Shiyinq <shiyinq.github.io@gmail.com>
2025-09-15 21:06:28 +0700

    style: add mcp-server folder

--- scripts/lint-format.sh
+++ scripts/lint-format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place src --exclude=__init__.py,_example
-isort src --profile black
-black src
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place src mcp-server --exclude=__init__.py,_example
+isort src mcp-server --profile black
+black src mcp-server
